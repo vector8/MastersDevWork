d9d3e3987e18797ea7df2bb51c335817a582c4d9
vector8 <vector8@users.noreply.github.com>
2018-06-14 16:37:16 -0400

    Added VikingVillage project.

---
+++ VikingVillage/Assets/DeviceSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceSwitcher : MonoBehaviour
+{
+    public enum Modes
+    {
+        Phidgets,
+        Arduino
+    }
+
+    public Modes mode;
+    public PhidgetComponent phidgetComponent;
+    public ArduinoComponent arduinoComponent;
+    public GameObject phidgetCallibrationPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        switch (mode)
+        {
+            case Modes.Phidgets:
+                arduinoComponent.enabled = false;
+                break;
+            case Modes.Arduino:
+                phidgetComponent.enabled = false;
+                phidgetCallibrationPanel.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+}
