aa1646c0fa4b3b4e5f3c564e45b9f8a5ff91a35d
vector8 <vector8@users.noreply.github.com>
2018-06-14 16:33:40 -0400

    Modified to find local min / max values to base the leg up triggers on.

--- ArduinoPressureSensor/ArduinoFSR/ArduinoFSR.ino
+++ ArduinoPressureSensor/ArduinoFSR/ArduinoFSR.ino
@@ -1,40 +1,69 @@
-#include <LinkedList.h>
-#include <QueueArray.h>
-
-const int leftFSRPin = A0;
-const int rightFSRPin = A1;
+const int leftFSRPin = A1;
+const int rightFSRPin = A0;
 
 int leftFsrValue = 0;  
 int rightFsrValue = 0;
 
-const float NUM_FRAMES = 50;
-const float THRESHOLD = 10;
+#define NUM_FRAMES 10
+const float THRESHOLD = 5;
 
-float sumRight;
-float sumLeft;
+struct Frame
+{
+  float value;
+  int life;
+};
 
-QueueArray<float> framesRight;
-QueueArray<float> framesLeft;
+float framesRight[NUM_FRAMES];
+float framesLeft[NUM_FRAMES];
+
+int currentIndex = 0;
 
 bool leftStep = true;
+bool first = true;
 
-float prevRunningAvgRight;
-float prevRunningAvgLeft;
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
+//void insertValue(float value, LinkedList<Frame> &frames)
+//{
+//  bool inserted = false;
+//  Frame newFrame;
+//  newFrame.value = value;
+//  newFrame.life = NUM_FRAMES;
+//  for(int i = 0; i < frames.size(); i++)
+//  {
+//    Frame f = frames.get(i);
+//
+//    f.life--;
+//
+//    if(f.life <= 0)
+//    {
+//      frames.remove(i);
+//      i--;
+//      continue;
+//    }
+//
+//    if(f.value >= value)
+//    {
+//      frames.add(i, newFrame);
+//      inserted = true;
+//      break;
+//    }
+//  }
+//  if(!inserted)
+//  {
+//   frames.add(newFrame);
+//  }
+//}
 
 void setup() 
 {
   Serial.begin(9600);
 
-  for(int i = 0; i < NUM_FRAMES; i++)
-  {
-    framesRight.push(0);
-    framesLeft.push(0);
-  }
-
-  sumRight = 0;
-  sumLeft = 0;
-  prevRunningAvgRight = 0;
-  prevRunningAvgLeft = 0;
+  
 }
 
 void loop() 
@@ -42,43 +71,76 @@ void loop()
   leftFsrValue = analogRead(leftFSRPin);
   rightFsrValue = analogRead(rightFSRPin);
 
-  float poppedRight = framesRight.pop();
-  float poppedLeft = framesLeft.pop();
-  framesRight.push(rightFsrValue);
-  framesLeft.push(leftFsrValue);
-
-  sumRight = (sumRight - poppedRight) + rightFsrValue;
-  sumLeft = (sumLeft - poppedLeft) + leftFsrValue;
-
-  float runningAvgRight = sumRight / NUM_FRAMES;
-  float runningAvgLeft = sumLeft / NUM_FRAMES;
-
-  if(rightFsrValue <= prevRunningAvgRight - THRESHOLD)
+  if(first)
   {
-    Serial.println("Right leg up");
+    memset(framesRight, rightFsrValue, NUM_FRAMES);
+    memset(framesLeft, leftFsrValue, NUM_FRAMES);
+    first = false;
   }
+  else
+  {  
+  //  insertValue(rightFsrValue, framesSortedRight);
+  //  insertValue(leftFsrValue, framesSortedLeft);
   
-  if(leftFsrValue <= prevRunningAvgLeft - THRESHOLD)
-  {
-    Serial.println("Left leg up");
+    currentIndex ++;
+    if(currentIndex >= NUM_FRAMES)
+      currentIndex = 0;
+    framesRight[currentIndex] = rightFsrValue;
+    framesLeft[currentIndex] = leftFsrValue;
+  
+    float rightMax = 0;
+    float rightMin = 1024;
+    float leftMax = 0;
+    float leftMin = 1024;
+  
+    for(int i = 0; i < NUM_FRAMES; i++)
+    {
+      if(framesRight[i] > rightMax)
+        rightMax = framesRight[i];
+      if(framesRight[i] < rightMin)
+        rightMin = framesRight[i];
+  
+      if(framesLeft[i] > leftMax)
+        leftMax = framesLeft[i];
+      if(framesLeft[i] < leftMin)
+        leftMin = framesLeft[i];
+    }
+  
+    float rightMid = (rightMin + (3.0f * rightMax)) / 4.0f;
+    float leftMid = (leftMin + (3.0f * leftMax)) / 4.0f;
+
+    int rightLeg = 1, leftLeg = 1;
+  
+    if(rightFsrValue <= rightMax - THRESHOLD)
+    {
+      //Serial.println("Right leg up");
+      rightLeg = 0;
+    }
+    
+    if(leftFsrValue <= leftMax - THRESHOLD)
+    {
+      //Serial.println("Left leg up");
+      leftLeg = 0;
+    }
+
+    Serial.println(String(rightLeg) + String(leftLeg));
+  //  if(leftStep && (leftFsrValue - rightFsrValue) > 100)
+  //  {
+  //    leftStep = false;
+  //    Serial.println("Left step!");
+  //  }
+  //  else if(!leftStep && (rightFsrValue - leftFsrValue) > 100)
+  //  {
+  //    leftStep = true;
+  //    Serial.println("Right step!");
+  //  }
+  
+    //String leftString = String(leftFsrValue);
+    //String rightString = String(rightFsrValue);
+    //Serial.println(leftString + "," + rightString + " : " + String(leftMid) + "," + String(rightMid));
+//    Serial.println(freeRam());
   }
-//  if(leftStep && (leftFsrValue - rightFsrValue) > 100)
-//  {
-//    leftStep = false;
-//    Serial.println("Left step!");
-//  }
-//  else if(!leftStep && (rightFsrValue - leftFsrValue) > 100)
-//  {
-//    leftStep = true;
-//    Serial.println("Right step!");
-//  }
-
-  String leftString = String(leftFsrValue);
-  String rightString = String(rightFsrValue);
-  Serial.println(leftString + "," + rightString);
-
-  prevRunningAvgRight = runningAvgRight;
-  prevRunningAvgLeft = runningAvgLeft;
 
   delay(100);
 }
+
